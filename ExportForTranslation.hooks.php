77d8902875b3f1c4ba5442dcfe74b38d6802853a
Dror S. [ffs] <FreedomFighterSparrow@gmail.com>
2017-02-14 19:33:18 +0200

    Add the export option to the editor toolbar

--- ExportForTranslation.hooks.php
+++ ExportForTranslation.hooks.php
@@ -7,5 +7,21 @@
  */
 
 class ExportForTranslationHooks {
+	public static function onSkinTemplateNavigation( SkinTemplate &$sktemplate, array &$links ) {
+		if ( $sktemplate->getUser()->isAllowed( 'export-for-translation' ) ) {
+			self::addButtonToToolbar( $sktemplate, $links );
+		}
 
+		return true;
+	}
+
+	public static function addButtonToToolbar( SkinTemplate &$sktemplate, array &$links ) {
+		$title = $sktemplate->getRelevantTitle();
+		$exportPage = SpecialPage::getTitleFor( 'ExportForTranslation', $title->getFullText() );
+		$links['actions']['exportfortranslation'] = [
+			'text'	=> $sktemplate->msg( 'exportfortranslation-btn' )->text(),
+			'href'	=>  $exportPage->getLocalURL()
+		];
+	}
 }
+
